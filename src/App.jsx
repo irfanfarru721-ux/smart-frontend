30b0b7e66d2214892034be2a0aa632b209d95055
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-30 01:01:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
-import Modules from "./pages/Modules.jsx";
-import Vendors from "./pages/Vendors.jsx";
-import Products from "./pages/Products.jsx";
+import Home from "./pages/Home.jsx";
+import ModulesPage from "./pages/ModulesPage.jsx";
+import VendorsPage from "./pages/VendorsPage.jsx";
+import ProductsPage from "./pages/ProductsPage.jsx";
+import ProductDetails from "./pages/ProductDetails.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import Cart from "./pages/Cart.jsx";
 import Checkout from "./pages/Checkout.jsx";
+import Orders from "./pages/Orders.jsx";
 
-export default function App() {
+export default function App(){
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Modules />} />
-        <Route path="/vendors/:moduleId" element={<Vendors />} />
-        <Route path="/products/:vendorId" element={<Products />} />
-        <Route path="/checkout" element={<Checkout />} />
+        <Route path='/' element={<Home/>} />
+        <Route path='/modules' element={<ModulesPage/>} />
+        <Route path='/modules/:moduleId/vendors' element={<VendorsPage/>} />
+        <Route path='/vendor/:vendorId/products' element={<ProductsPage/>} />
+        <Route path='/product/:id' element={<ProductDetails/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<Signup/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/checkout' element={<Checkout/>} />
+        <Route path='/orders' element={<Orders/>} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
