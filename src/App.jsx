656d30f6c30f66aa6955cd83d4a2b98f6e67e6fb
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-29 22:36:57 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import ProductDetails from "./pages/ProductDetails";
-import Vendor from "./pages/Vendor";
-import Categories from "./pages/Categories";
-import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
-import Orders from "./pages/Orders";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import ModulesPage from "./pages/ModulesPage.jsx";
+import VendorsPage from "./pages/VendorsPage.jsx";
+import ProductsPage from "./pages/ProductsPage.jsx";
+import ProductDetails from "./pages/ProductDetails.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import Cart from "./pages/Cart.jsx";
+import Checkout from "./pages/Checkout.jsx";
+import Orders from "./pages/Orders.jsx";
 
-export default function App() {
+export default function App(){
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="/vendor/:id" element={<Vendor />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/orders" element={<Orders />} />
+        <Route path='/' element={<Home/>} />
+        <Route path='/modules' element={<ModulesPage/>} />
+        <Route path='/modules/:moduleId/vendors' element={<VendorsPage/>} />
+        <Route path='/vendor/:vendorId/products' element={<ProductsPage/>} />
+        <Route path='/product/:id' element={<ProductDetails/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<Signup/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/checkout' element={<Checkout/>} />
+        <Route path='/orders' element={<Orders/>} />
       </Routes>
     </>
   );
