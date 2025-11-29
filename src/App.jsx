841e22431a897b0cfc0b6a1d49e32f8aeacdb72b
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-29 20:15:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,36 @@ import Modules from "./pages/Modules.jsx";
 import Vendors from "./pages/Vendors.jsx";
 import Products from "./pages/Products.jsx";
 import Checkout from "./pages/Checkout.jsx";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import AdminDashboard from "./pages/AdminDashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
+
       <Routes>
+        {/* Public pages */}
         <Route path="/" element={<Modules />} />
         <Route path="/vendors/:moduleId" element={<Vendors />} />
         <Route path="/products/:vendorId" element={<Products />} />
         <Route path="/checkout" element={<Checkout />} />
+
+        {/* Auth pages */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Admin protected page */}
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
