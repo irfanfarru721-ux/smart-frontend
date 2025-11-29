6ee51f3d1530c57981e321fa405d72e2a9ec2eaf
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-29 22:28:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,23 @@ import Modules from "./pages/Modules.jsx";
 import Vendors from "./pages/Vendors.jsx";
 import Products from "./pages/Products.jsx";
 import Checkout from "./pages/Checkout.jsx";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Modules />} />
         <Route path="/vendors/:moduleId" element={<Vendors />} />
         <Route path="/products/:vendorId" element={<Products />} />
+
+        {/* Auth Routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
         <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </BrowserRouter>
