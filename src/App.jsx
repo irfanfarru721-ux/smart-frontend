71a03bfccb99a71916266263efc621cbe2cf6cba
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-30 03:03:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+
 import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
 import Modules from "./pages/Modules.jsx";
 import Vendors from "./pages/Vendors.jsx";
 import Products from "./pages/Products.jsx";
@@ -8,14 +13,22 @@ import Checkout from "./pages/Checkout.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Modules />} />
-        <Route path="/vendors/:moduleId" element={<Vendors />} />
-        <Route path="/products/:vendorId" element={<Products />} />
-        <Route path="/checkout" element={<Checkout />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {/* PUBLIC */}
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+
+          {/* MODULE/VENDOR/PRODUCT PAGES */}
+          <Route path="/modules" element={<Modules />} />
+          <Route path="/vendors/:moduleId" element={<Vendors />} />
+          <Route path="/products/:vendorId" element={<Products />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
