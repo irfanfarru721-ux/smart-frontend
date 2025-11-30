a365d1c0bdcf6df3f09bfa623b5c186265619d63
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-30 13:56:46 +0530

    Delete src/api/modules.js

--- src/api/modules.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API_URL = import.meta.env.VITE_API_URL;
-
-// Fetch all main modules (Restaurant, Grocery…)
-export const fetchModules = async () => {
-  const res = await axios.get(`${API_URL}/modules`);
-  return res.data;
-};
