894ab7261c3f6fba154d63c5001e5db7f85e197c
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-30 13:56:58 +0530

    Delete src/api/products.js

--- src/api/products.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API_URL = import.meta.env.VITE_API_URL;
-
-// Fetch products of a vendor
-export const fetchProductsByVendor = async (vendorId) => {
-  const res = await axios.get(`${API_URL}/products/vendor/${vendorId}`);
-  return res.data;
-};
