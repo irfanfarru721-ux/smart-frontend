34a40c0c81d97bed234319d4b67ea0c29233c7ee
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-30 14:30:55 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css"; // optional, if you have global styles
+import App from "./App.jsx"; // make sure this path is correct
+import "./index.css"; // optional
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
