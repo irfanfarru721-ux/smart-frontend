a57b35926199011ca426a0d349cf41bccd254044
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-30 14:11:35 +0530

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,44 +1,25 @@
 import axios from "axios";
 
-export const registerUser = async ({ name, email, password }) => {
+export const signup = async ({ name, email, password }) => {
   try {
-    const res = await axios({
-      method: "POST",
-      url:"https://srudentbackend-1.onrender.com/api/auth/register",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: JSON.stringify({
-        name,
-        email,
-        password,
-      }),
-    });
-
+    const res = await axios.post(
+      "https://srudentbackend-1.onrender.com/api/auth/register",
+      { name, email, password }
+    );
     return res.data;
   } catch (err) {
-    console.log("REGISTER ERROR:", err?.response?.data || err.message);
     return { error: err?.response?.data?.message || "Registration failed" };
   }
 };
 
-export const loginUser = async ({ email, password }) => {
+export const login = async ({ email, password }) => {
   try {
-    const res = await axios({
-      method: "POST",
-      url: "https://multi-vendor-app-ey66.onrender.com/api/auth/login",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: JSON.stringify({
-        email,
-        password,
-      }),
-    });
-
+    const res = await axios.post(
+      "https://multi-vendor-app-ey66.onrender.com/api/auth/login",
+      { email, password }
+    );
     return res.data;
   } catch (err) {
-    console.log("LOGIN ERROR:", err?.response?.data || err.message);
     return { error: err?.response?.data?.message || "Login failed" };
   }
 };
