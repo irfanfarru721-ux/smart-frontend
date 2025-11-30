22eecedfd41b93429389f0136f3f2a974ad87f31
irfanfarru721-ux <irfanfarru721@gmail.com>
2025-11-30 14:39:14 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx"; // make sure this path is correct
+import App from "./App.jsx";
 import "./index.css"; // optional
 
 ReactDOM.createRoot(document.getElementById("root")).render(
